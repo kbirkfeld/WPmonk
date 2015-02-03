990e89bf9113d95c277246d9be6498702f7f0464
Kyle Birkfeld <kbirkfeld@gmail.com>
2015-02-03 13:24:15 -0800

    Added if/else for submission to notify if request was completed

--- import-submit.php
+++ import-submit.php
@@ -10,6 +10,13 @@ $items = getWpPosts($fileName);
 $convertedItems = BlogPost::convert_wp_blogpost($items, $blogId);
 
 foreach($convertedItems as $convertedItem) {
-    BlogPost::create($blogId, $convertedItem);
+    $posts .= BlogPost::create($blogId, $convertedItem);
 }
+
+if (isset($posts)){
+    echo "Your import was successful";
+} else {
+    echo "Please try your import again";
+}
+
 ?>
